33c01a456041d4410029308ec16bc9288f81cee0
Gandjurov <Gandjurov@abv.bg>
2018-10-20 12:23:26 +0300

    Add Stacks and Queues

---
+++ 01. Stacks and Queues/CSharpAdvanced/04. BasicQueueOperations/BasicQueueOperations.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BasicQueueOperations
+{
+    class BasicQueueOperations
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> queue = new Queue<int>();
+
+            int N = numbers[0];
+            int S = numbers[1];
+            int X = numbers[2];
+
+            var inputSecondLine = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            for (int i = 0; i < N; i++)
+            {
+                queue.Enqueue(inputSecondLine[i]);
+            }
+
+            for (int i = 0; i < S; i++)
+            {
+                queue.Dequeue();
+            }
+
+            if (queue.Count != 0)
+            {
+                if (queue.Contains(X))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine(queue.Min());
+                }
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+
+        }
+    }
+}
